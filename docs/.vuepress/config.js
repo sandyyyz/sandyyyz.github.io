07bad0981b51181865db1ec38ca553f793b4299d
zoe <sandyyyz@outlook.com>
2025-11-02 22:58:25 +0800

    fix(config): Fix config bugs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,34 +1,32 @@
 const head = require('./config/head.js');
-const plugins = require('./config/plugins.js');
 const themeConfig = require('./config/themeConfig.js');
 
-module.exports = {
-  theme: 'vdoing', // 使用npm包主题
-  title: "sandyyyz-notes",
-  description: 'vdoing博客主题模板',
-  base: '/sandyyyz-notes/', // 格式：'/<仓库名>/'， 默认'/'
-  markdown: {
-    lineNumbers: true, // 代码行号
-  },
-
-  head,
-  plugins,
-  themeConfig,
-}
-// vssue 评论插件
-plugins: [
+const plugins = [
+  ...require('./config/plugins.js'),
   [
     "vuepress-plugin-vssue-global",
     {
       platform: "github",
       title: "[Comment]<%- frontmatter.title %>",
       needComments: true,
-      // 其他的 Vssue 配置
       autoCreateIssue: true,
       clientId: "Ov23liGQDVSA9fvCIqRm",
       clientSecret: "e79eaa54d211ab06e2f7bc260507eb153af0ff8f",
-      owner: "lql95",
-      repo: "lql-notes",
+      owner: "sandyyyz",          // ✅ 改成你自己的用户名
+      repo: "sandyyyz-notes",     // ✅ 改成你的博客仓库名
     },
   ],
-]
+];
+
+module.exports = {
+  theme: 'vdoing',
+  title: "sandyyyz-notes",
+  description: 'vdoing博客主题模板',
+  base: '/', // ✅ 或者 '/'，取决于仓库名
+  markdown: {
+    lineNumbers: true,
+  },
+  head,
+  plugins,
+  themeConfig,
+};
