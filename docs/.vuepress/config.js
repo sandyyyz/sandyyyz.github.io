7a27e062bc87fdfde1119224746dc938f502a00d
zoe <sandyyyz@outlook.com>
2025-11-02 22:40:34 +0800

    feat(command): Add command module

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,9 +4,9 @@ const themeConfig = require('./config/themeConfig.js');
 
 module.exports = {
   theme: 'vdoing', // 使用npm包主题
-  title: "vdoing-template",
+  title: "sandyyyz-notes",
   description: 'vdoing博客主题模板',
-  base: '/vdoing-template/', // 格式：'/<仓库名>/'， 默认'/'
+  base: '/sandyyyz-notes/', // 格式：'/<仓库名>/'， 默认'/'
   markdown: {
     lineNumbers: true, // 代码行号
   },
@@ -15,3 +15,20 @@ module.exports = {
   plugins,
   themeConfig,
 }
+// vssue 评论插件
+plugins: [
+  [
+    "vuepress-plugin-vssue-global",
+    {
+      platform: "github",
+      title: "[Comment]<%- frontmatter.title %>",
+      needComments: true,
+      // 其他的 Vssue 配置
+      autoCreateIssue: true,
+      clientId: "Ov23liGQDVSA9fvCIqRm",
+      clientSecret: "e79eaa54d211ab06e2f7bc260507eb153af0ff8f",
+      owner: "lql95",
+      repo: "lql-notes",
+    },
+  ],
+]
