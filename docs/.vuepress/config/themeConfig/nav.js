dfc0ba933040d99d322ebcd43db26188536211db
zoe <sandyyyz@outlook.com>
2025-11-03 17:31:30 +0800

    chore(test): Test

--- docs/.vuepress/config/themeConfig/nav.js
+++ docs/.vuepress/config/themeConfig/nav.js
@@ -5,6 +5,5 @@ module.exports = [
   { text: '分布式',link: '/分布式/',},
   { text: '工具链', link: '/工具链/' },
   { text: '留言板', link: '/message-board/' },
-  { text: '我的博客', link: 'https://wiki.eryajf.net' },
   { text: 'Github', link: 'https://github.com/sandyyyz' },
 ]
